19ed44a5dfa43d53c2415085d1efd15e103b354a
Michael Osipov <1983-01-06@gmx.net>
2013-02-01 20:31:40 +0000

    Added two Tomcat-based tests on servlets and URL connections

--- src/test/java/net/sf/michaelo/dirctxsrc/DirContextSourceFactoryTest.java
+++ src/test/java/net/sf/michaelo/dirctxsrc/DirContextSourceFactoryTest.java
@@ -16,40 +16,132 @@
 package net.sf.michaelo.dirctxsrc;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 import org.apache.catalina.startup.Bootstrap;
-import org.junit.After;
-import org.junit.Before;
+import org.apache.commons.io.IOUtils;
+import org.apache.directory.server.constants.ServerDNConstants;
+import org.apache.directory.server.core.DefaultDirectoryService;
+import org.apache.directory.server.core.DirectoryService;
+import org.apache.directory.server.core.partition.impl.btree.jdbm.JdbmPartition;
+import org.apache.directory.server.core.partition.ldif.LdifPartition;
+import org.apache.directory.server.core.schema.SchemaPartition;
+import org.apache.directory.server.ldap.LdapServer;
+import org.apache.directory.server.protocol.shared.transport.TcpTransport;
+import org.apache.directory.shared.ldap.schema.SchemaManager;
+import org.apache.directory.shared.ldap.schema.ldif.extractor.SchemaLdifExtractor;
+import org.apache.directory.shared.ldap.schema.ldif.extractor.impl.DefaultSchemaLdifExtractor;
+import org.apache.directory.shared.ldap.schema.loader.ldif.LdifSchemaLoader;
+import org.apache.directory.shared.ldap.schema.manager.impl.DefaultSchemaManager;
+import org.apache.directory.shared.ldap.schema.registries.SchemaLoader;
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 // $Id$
 public class DirContextSourceFactoryTest {
 	
-	private Bootstrap bootstrap;
+	private static Bootstrap bootstrap;
 	private static File catalinaHome;
+	private static DirectoryService directoryService;
+	private static LdapServer ldapServer;
 	
 	@BeforeClass
-	public static void prepareEnv() {
+	public static void prepareEnv() throws Exception {
 		String buildDirectory = System.getProperty("buildDirectory");
+
+		/*
+		 * Code taken and adapted from
+		 * http://svn.apache.org/repos/asf/directory/documentation/samples/trunk/embedded-sample/
+		 */
+		File workingDirectory = new File(buildDirectory, "apacheds-work");
+		workingDirectory.mkdir();
+
+		directoryService = new DefaultDirectoryService();
+		directoryService.setWorkingDirectory(workingDirectory);
+
+		SchemaPartition schemaPartition = directoryService.getSchemaService()
+				.getSchemaPartition();
+
+		LdifPartition ldifPartition = new LdifPartition();
+		File schemaRepository = new File(workingDirectory, "schema");
+		ldifPartition.setWorkingDirectory(schemaRepository.getPath());
+
+		SchemaLdifExtractor extractor = new DefaultSchemaLdifExtractor(
+				workingDirectory);
+		extractor.extractOrCopy(true);
+
+		schemaPartition.setWrappedPartition(ldifPartition);
+
+		SchemaLoader loader = new LdifSchemaLoader(schemaRepository);
+		SchemaManager schemaManager = new DefaultSchemaManager(loader);
+		directoryService.setSchemaManager(schemaManager);
+
+		schemaManager.loadAllEnabled();
+
+		schemaPartition.setSchemaManager(schemaManager);
+
+		List<Throwable> errors = schemaManager.getErrors();
+
+		if (!errors.isEmpty())
+			throw new Exception("Schema load failed: " + errors);
+
+		JdbmPartition systemPartition = new JdbmPartition();
+		systemPartition.setId("system");
+		systemPartition.setPartitionDir(new File(directoryService
+				.getWorkingDirectory(), "system"));
+		systemPartition.setSuffix(ServerDNConstants.SYSTEM_DN);
+		systemPartition.setSchemaManager(schemaManager);
+		directoryService.setSystemPartition(systemPartition);
+
+		directoryService.setShutdownHookEnabled(false);
+		directoryService.getChangeLog().setEnabled(false);
+
+		ldapServer = new LdapServer();
+		ldapServer.setTransports(new TcpTransport(11389));
+		ldapServer.setDirectoryService(directoryService);
+
+		directoryService.startup();
+		ldapServer.start();
+		
 		catalinaHome = new File(new File(buildDirectory, "test-classes"), "tomcat6x-home");
-	}
-	
-	@Before
-	public void startTomcat() throws Exception {
 		bootstrap = new Bootstrap();
 		bootstrap.setCatalinaHome(catalinaHome.getAbsolutePath());
 		bootstrap.start();
 	}
 	
-	@After
-	public void stopTomcat() throws Exception {
+	@AfterClass
+	public static void stopEnv() throws Exception {
 		bootstrap.stop();
+		ldapServer.stop();
+		directoryService.shutdown();
+		directoryService.getWorkingDirectory().delete();
 	}
 	
 	@Test
-	public void test() {
-		System.out.println(bootstrap.getCatalinaBase());
+	public void callDirContextSourceFromServlet() throws IOException {
+		URL url = new URL("http://localhost:28888/dircontextsource/apacheds");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.connect();
+		InputStream is  = conn.getInputStream();
+		String content = IOUtils.toString(is, "UTF-8");
+		System.out.println("Received from URL: " + content);
+		Assert.assertNotNull(content);
+		conn.disconnect();
+	}
+	
+	@Test
+	public void callDeadDirContextSourceFromServlet() throws IOException {
+		URL url = new URL("http://localhost:28888/dircontextsource/dead");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.connect();
+		Assert.assertEquals(500, conn.getResponseCode());
+		conn.disconnect();
 	}
 
 }
