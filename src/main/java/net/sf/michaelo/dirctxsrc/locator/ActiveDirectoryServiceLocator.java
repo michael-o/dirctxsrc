db94d9aa7c6071756ba1069c6bd099b75b7ba251
Michael Osipov <1983-01-06@gmx.net>
2016-04-22 14:55:33 +0000

    Cleanup and properties

--- src/main/java/net/sf/michaelo/dirctxsrc/locator/ActiveDirectoryServiceLocator.java
+++ src/main/java/net/sf/michaelo/dirctxsrc/locator/ActiveDirectoryServiceLocator.java
@@ -1,9 +1,7 @@
 package net.sf.michaelo.dirctxsrc.locator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Logger;
