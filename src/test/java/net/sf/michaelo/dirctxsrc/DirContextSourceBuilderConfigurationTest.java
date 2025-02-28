f9c5017e27fb43c4f8319a3f2758b2ead76aa22c
Michael Osipov <1983-01-06@gmx.net>
2025-02-28 18:32:47 +0100

    Add missing test method

--- src/test/java/net/sf/michaelo/dirctxsrc/DirContextSourceBuilderConfigurationTest.java
+++ src/test/java/net/sf/michaelo/dirctxsrc/DirContextSourceBuilderConfigurationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013–2021 Michael Osipov
+ * Copyright 2013–2025 Michael Osipov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -120,6 +120,11 @@ public class DirContextSourceBuilderConfigurationTest {
 		assertNotNull(newInstance().binaryAttributes("objectGuid", "objectSid"));
 	}
 
+	@Test
+	public void configureReferral() {
+		assertNotNull(newInstance().referral("ignore"));
+	}
+	
 	@Test
 	public void configureAdditionalProperty() {
 		assertNotNull(newInstance().additionalProperty("magic", "shazam"));
